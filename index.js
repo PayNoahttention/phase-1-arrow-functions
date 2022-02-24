b83fb37ed9c03011f9341a5bd0fa686654b5431f
NoahSolis <NSolis1993@gmail.com>
2022-02-24 00:08:48 -0600

    commit tree 2142d1f1bd78bca7c9f3059a55bff6ac159e79cc

---
+++ index.js
@@ -0,0 +1,5 @@
+const divide = (parameter1 = 2000 , parameter2 = 100) => parameter1 / parameter2;
+
+const square = (parameter1) => parameter1 ** parameter1;
+
+const add = (parameter1 = 2, parameter2 = 4) =>  parameter1 + parameter2;
